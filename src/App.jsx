5461e42a975cd5355f5978b6d1f6de53000eb28e
thearuniyer <ashank16@asu.edu>
2023-02-22 17:21:02 +0530

    Changed react-router-dom to v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Header from './components/Header'
-import NotesListPage from './pages/NotesListPage'
+import {
+  Route,
+  BrowserRouter as Router
+} from "react-router-dom";
+import './App.css';
+import Header from './components/Header';
+import NotePage from './pages/NotePage';
+import NotesListPage from './pages/NotesListPage';
 
 function App() {
 
   return (
-    <div className='App'>
-      <Header />
-      <NotesListPage />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+          <Route path="/" exact component={NotesListPage} />
+          <Route path="/note/:id" component={NotePage} />
+      </div>
+    </Router>
   )
 }
 
--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const NotePage = () => {
+const NotePage = (props) => {
+  console.log("props:", props);
   return (
     <div>
         <h1>This is a single note page</h1>
